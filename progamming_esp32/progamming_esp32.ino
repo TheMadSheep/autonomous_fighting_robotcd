8388e8dbbc6c30ce5ebe01dcec8c2ba49dc5d773
TheMadSheep <eduardo31415926535@gmail.com>
2025-11-10 16:11:16 -0300

    back

--- progamming_esp32/progamming_esp32.ino
+++ progamming_esp32/progamming_esp32.ino
@@ -1,68 +1,10 @@
 
-// as especifacoes dos dois cores do ESP32 sao basicamente 
-// iguais, porem o core 0 é dedicado ao sistema operacional 
-// (FreeRTOS) e o core 1 é dedicado ao usuario. Para usarmos o core 0
-// precisamos usar a funcao xTaskCreatePinnedToCore() para criar
-// uma tarefa (task) e "prendê-la" ao core 0. Ja o core 1
-// pode ser usado normalmente na funcao loop() ou com tasks. Isso influencia no processamento
-// paralelo, ja que o core 0 pode ficar sobrecarregado com tarefas do sistema operacional.
-
-TaskHandle_t Task1;
-TaskHandle_t Task2;
-
-const int led1 = 2;
-const int led2 = 4;
-
 void setup() {
-  Serial.begin(115200); 
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
+  // put your setup code here, to run once:
 
-  xTaskCreatePinnedToCore(
-                    Task1code,   /* Task function. */
-                    "Task1",     /* name of task. */
-                    10000,       /* Stack size of task */
-                    NULL,        /* parameter of the task */
-                    1,           /* priority of the task */
-                    &Task1,      /* Task handle to keep track of created task */
-                    0);          /* pin task to core 0 */                  
-  delay(500); 
-
- xTaskCreatePinnedToCore(
-                    Task2code,   /* Task function. */
-                    "Task2",     /* name of task. */
-                    10000,       /* Stack size of task */
-                    NULL,        /* parameter of the task */
-                    1,           /* priority of the task */
-                    &Task2,      /* Task handle to keep track of created task */
-                    1);          /* pin task to core 1 */
-    delay(500); 
-}
-
-void Task1code( void * pvParameters ){
-  Serial.print("Task1 running on core ");
-  Serial.println(xPortGetCoreID());
-
-  for(;;){
-    digitalWrite(led1, HIGH);
-    delay(1000);
-    digitalWrite(led1, LOW);
-    delay(1000);
-  } 
-}
-
-void Task2code( void * pvParameters ){
-  Serial.print("Task2 running on core ");
-  Serial.println(xPortGetCoreID());
-
-  for(;;){
-    digitalWrite(led2, HIGH);
-    delay(700);
-    digitalWrite(led2, LOW);
-    delay(700);
-  }
 }
 
 void loop() {
-  
+  // put your main code here, to run repeatedly:
+
 }
